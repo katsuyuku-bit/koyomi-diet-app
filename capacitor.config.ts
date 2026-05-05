d8405b2bd5952ebc5a1e59ca4d65265f206d7617
katsuyuku-bit <katsuyuku@gmail.com>
2026-05-05 17:37:42 +0900

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,5 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
 const config: CapacitorConfig = {
-  appId: "com.example.app",
+  appId: "com.example.koyomidiet",
   appName: "きみとダイエット",
   webDir: "out",
 };
+
+export default config;
